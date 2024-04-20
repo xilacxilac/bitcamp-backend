b846af15a348d3ec7583a8d522bd037360699089
The-Vinh Calix Tran-Luu <xilacxilac@gmail.com>
2024-04-20 17:59:58 -0400

    beenis

--- app.py
+++ app.py
@@ -1,33 +1,24 @@
-from flask import Flask, jsonify
-import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import firestore
+import os
+
+from flask import Flask
+from pymongo.mongo_client import MongoClient
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
+uri = os.environ.get('MONGO_URI')
+client = MongoClient(uri)
 
-cred = credentials.Certificate("key.json")
-firebase_admin.initialize_app(cred)
-db = firestore.client()
-
-@app.route('/data', methods=['GET'])
-def get_data():
-    # Example: Get data from Firestore
-    data = db.collection('collection_name').document('document_id').get().to_dict()
-    return jsonify(data)
-
-@app.route('/data', methods=['POST'])
-def add_data():
-    # Example: Add data to Firestore
-    new_data = {'key': 'value'}
-    db.collection('collection_name').document().set(new_data)
-    return jsonify({'message': 'Data added successfully'})
+db = client['bitcamp2024']
+collection = db["pp"]
 
 test = {"name": "bob", "data": "hello world"}
 
 @app.route('/')
 def hello_world():
-    add_data()
+    try:
+        collection.insert_one(test)
+    except Exception as e:
+        print(e)
     return 'Hello World!'
-
-if __name__ == '__main__':
-    app.run()
