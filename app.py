938ee9af04980504f34ba9656562dc66b2d27f78
The-Vinh Calix Tran-Luu <xilacxilac@gmail.com>
2024-04-20 19:26:49 -0400

    base chore

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 import os
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, json, request
 from flask_cors import CORS
 from pymongo.mongo_client import MongoClient
 from dotenv import load_dotenv
+
+from Chore import Chore
 
 load_dotenv()
 
@@ -15,16 +17,39 @@ client = MongoClient(uri)
 db = client['bitcamp2024']
 collection = db["pp"]
 
-test = {"name": "bob", "data": "hello world"}
+
+@app.route("/addchore", methods=['POST'])
+def add_chore():
+    group_name = request.args.get('group_name', default="", type=str)
+    chore = Chore(request.args)
+
+    if group_name != "":
+        try:
+            client['chores'][group_name].insert_one({"chore": chore})
+        except Exception as e:
+            print(e)
+    else:
+        print("No group defined")
+
+@app.route("/getchore", methods=['GET'])
+def get_chore():
+    return
+
+
+@app.route("/deletechore", methods=['POST'])
+def delete_chore():
+    return
+
 
 @app.route('/test')
 def test():
-    return jsonify(test)
+    return json.dumps({"name": "bob", "data": "hello world"})
+
 
 @app.route('/')
-def hello_world():
+def index():
     try:
-        collection.insert_one(test)
+        collection.insert_one({"name": "bob", "data": "hello world"})
     except Exception as e:
         print(e)
-    return 'Hello World!'
+    return "Backend is Up"
