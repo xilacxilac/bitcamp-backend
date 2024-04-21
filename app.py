722a5d39f7b6b1bcfc70e8bfaf9e35f25f8baf01
The-Vinh Calix Tran-Luu <xilacxilac@gmail.com>
2024-04-20 21:13:11 -0400

    fix returns

--- app.py
+++ app.py
@@ -29,10 +29,13 @@ def add_chore():
     if group_name != "":
         try:
             client['chores'][group_name].insert_one({"chore": chore, "due_date": dt, "chore_id": uuid.uuid4()})
+            return "200"
         except Exception as e:
             print(e)
+            return "400"
     else:
         print("No group defined")
+        return "400"
 
 
 @app.route("/getchorebyid", methods=['GET'])
@@ -44,42 +47,55 @@ def get_chore_by_id():
             return client['chores'][group_name].find({"chore_id": request.args.get('chore_id', type=str)})
         except Exception as e:
             print(e)
+            return "400"
     else:
         print("No group defined")
+        return "400"
 
 
 @app.route("/getchoretoday", methods=['GET'])
 def get_chore_today():
     group_name = request.args.get('group_name', default="", type=str)
 
-    # gets chores that are due today
-    now = datetime.now()
-    start_of_today = datetime(now.year, now.month, now.day)
-    end_of_today = start_of_today + timedelta(days=1)
+    if group_name != "":
+        try:
+            now = datetime.now()
+            start_of_today = datetime(now.year, now.month, now.day)
+            end_of_today = start_of_today + timedelta(days=1)
 
-    # Query to find chores due today
-    query = {"due_date": {"$gte": start_of_today, "$lt": end_of_today}}
-    chores_due_today = client['chores'][group_name].find(query)
-
-    result = list(chores_due_today)
-    return result
+            query = {"due_date": {"$gte": start_of_today, "$lt": end_of_today}}
+            chores_due_today = client['chores'][group_name].find(query)
+            result = list(chores_due_today)
+            return result
+        except Exception as e:
+            print(e)
+            return "400"
+    else:
+        print("No group defined")
+        return "400"
 
 
 @app.route("/getchoretomorrow", methods=['GET'])
 def get_chore_tomorrow():
     group_name = request.args.get('group_name', default="", type=str)
 
-    # gets chores that are due today
-    now = datetime.now()
-    start_of_today = datetime(now.year, now.month, now.day) + timedelta(days=1)
-    end_of_today = start_of_today + timedelta(days=2)
+    if group_name != "":
+        try:
+            now = datetime.now()
+            start_of_today = datetime(now.year, now.month, now.day) + timedelta(days=1)
+            end_of_today = start_of_today + timedelta(days=2)
 
-    # Query to find chores due today
-    query = {"due_date": {"$gte": start_of_today, "$lt": end_of_today}}
-    chores_due_today = client['chores'][group_name].find(query)
+            query = {"due_date": {"$gte": start_of_today, "$lt": end_of_today}}
+            chores_due_today = client['chores'][group_name].find(query)
 
-    result = list(chores_due_today)
-    return result
+            result = list(chores_due_today)
+            return result
+        except Exception as e:
+            print(e)
+            return "400"
+    else:
+        print("No group defined")
+        return "400"
 
 
 @app.route("/deletechore", methods=['POST'])
@@ -89,10 +105,13 @@ def delete_chore():
     if group_name != "":
         try:
             client['chores'][group_name].delete_one({"chore_id": request.args.get('chore_id', type=str)})
+            return "200"
         except Exception as e:
             print(e)
+            return "400"
     else:
         print("No group defined")
+        return "400"
 
 
 @app.route('/test')
